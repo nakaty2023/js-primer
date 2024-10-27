be8b35dd048a189c8cf8783a09d2e686c3668880
nakaty <78027524+nakaty2023@users.noreply.github.com>
2024-10-27 10:53:31 +0900

    Node.jsでCLIアプリケーション / MarkdownをHTMLに変換する

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 import * as util from "node:util";
 import * as fs from "node:fs/promises";
+import { marked } from "marked";
 
-const { positionals } = util.parseArgs({
-    allowPositionals: true
+const {
+    values,
+    positionals
+} = util.parseArgs({
+    allowPositionals: true,
+    options: {
+        gfm: {
+            type: "boolean",
+            default: false,
+        }
+    }
 });
 const filePath = positionals[0];
 fs.readFile(filePath, { encoding: "utf8" }).then(file => {
-    console.log(file);
+    const html = marked.parse(file, {
+        gfm: values.gfm
+    });
+    console.log(html);
 }).catch(err => {
     console.error(err.message);
     process.exit(1);
